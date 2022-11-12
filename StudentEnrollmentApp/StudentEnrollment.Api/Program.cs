2cebfb39de448abbc2b7f82e6bd0e4555676ab82
Cristi Teodorescu <cristian.teodorescu97@yahoo.ro>
2022-11-12 18:32:09 +0200

    Add AllowCors

--- StudentEnrollmentApp/StudentEnrollment.Api/Program.cs
+++ StudentEnrollmentApp/StudentEnrollment.Api/Program.cs
@@ -4,6 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy => policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+});
 
 var app = builder.Build();
 
@@ -15,6 +19,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 var summaries = new[]
 {
