6ea30f16fb704f77536a81cd48e14c352fe20476
Cristi Teodorescu <cristian.teodorescu97@yahoo.ro>
2022-11-20 10:50:47 +0200

    Add automapper to my project

--- StudentEnrollmentApp/StudentEnrollment.Api/Program.cs
+++ StudentEnrollmentApp/StudentEnrollment.Api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StudentEnrollment.Data;
 using StudentEnrollment.Api.Endpoints;
+using StudentEnrollment.Api.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,7 @@ builder.Services.AddDbContext<StudentEnrollmentDbContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(MapperConfig));
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy => policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
